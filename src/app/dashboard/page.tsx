55ee29260052d0431378e958bb1b1aefd579a879
hakan5288 <hassanxh001@gmail.com>
2025-05-10 17:02:17 +0500

    Update page.tsx to not use SSR

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -61,6 +61,8 @@
 import DashboardPage from '@/features/users/components/dashboar-page'
 import React from 'react'
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <DashboardPage />
