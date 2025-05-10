0c23c7ff9646d1707f3eebd1fa33c10e4c103039
hakan5288 <hassanxh001@gmail.com>
2025-05-10 16:38:18 +0500

    Delete client directory

--- client/src/app/dashboard/favourites/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import FavouriteLocations from '@/features/users/components/favourite-locations'
-
-export default function Page() {
-  return (
-    <FavouriteLocations />
-  )
-}
