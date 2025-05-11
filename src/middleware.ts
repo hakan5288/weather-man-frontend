65182b1d3ab95b98006e10b265b778a57201df9d
hakan5288 <hassanxh001@gmail.com>
2025-05-11 09:59:00 +0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,24 +2,24 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  const jwtCookie = request.cookies.get("token");
+  // const { pathname } = request.nextUrl;
+  // const jwtCookie = request.cookies.get("token");
 
-  const isLoggedIn = !!jwtCookie;
+  // const isLoggedIn = !!jwtCookie;
 
-  const isAuthRoute = pathname.startsWith("/auth") || pathname === "/";
-  const isDashboardRoute = pathname.startsWith("/dashboard");
+  // const isAuthRoute = pathname.startsWith("/auth") || pathname === "/";
+  // const isDashboardRoute = pathname.startsWith("/dashboard");
 
-  if (isLoggedIn && isAuthRoute) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+  // if (isLoggedIn && isAuthRoute) {
+  //   return NextResponse.redirect(new URL("/dashboard", request.url));
+  // }
 
-  if (!isLoggedIn && isDashboardRoute) {
-    return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
-  if(!isLoggedIn && pathname === "/"){
-    return NextResponse.redirect(new URL("/auth/login" , request.url))
-  }
+  // if (!isLoggedIn && isDashboardRoute) {
+  //   return NextResponse.redirect(new URL("/auth/login", request.url));
+  // }
+  // if(!isLoggedIn && pathname === "/"){
+  //   return NextResponse.redirect(new URL("/auth/login" , request.url))
+  // }
 
   // Allow request to proceed
   return NextResponse.next();
